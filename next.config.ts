d8a05511542a84cec9c53fbb3197adb889cc2831
Jkgl2026 <nnnnnn0321@126.com>
2026-01-31 10:49:47 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 import path from 'path';
-import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
-  // outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  /* config options here */
-  // 允许所有域名访问（开发环境）
-  // allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
       {
@@ -16,17 +11,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // 添加空的 turbopack 配置以解决 Turbopack 和 Webpack 冲突问题
   turbopack: {},
 };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  sw: 'sw.js',
-  scope: '/',
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
